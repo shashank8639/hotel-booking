9a38c0ea74eda9f2716dbc6ed6fa6bed53c44cc6
E Shivashashank <eshivashashank@Es-MacBook-Air.local>
2026-05-05 10:00:00 +0530

    Initial project structure

---
+++ backend/src/main/java/com/hotelbooking/HotelBookingApplication.java
@@ -0,0 +1,14 @@
+package com.hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class HotelBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingApplication.class, args);
+    }
+}
